2fe7670ad1ab11dfb6498a3c1f583cdb08b9045f
phanleil-blip <phanleil@hawaii.edu>
2026-06-28 13:41:03 -1000

    Update update_KaniMoana.sh to include schedule.wpi
    Added download and copy for schedule.wpi file.

--- scripts/update_KaniMoana.sh
+++ scripts/update_KaniMoana.sh
@@ -11,4 +11,7 @@ sudo chmod +x audio_recording.sh
 sudo wget -O shutdown_now.sh https://raw.githubusercontent.com/phanleil-blip/KaniMoana/main/scripts/shutdown_now.sh
 sudo chmod +x shutdown_now.sh
 
+sudo wget -O config/schedule.wpi https://raw.githubusercontent.com/phanleil-blip/KaniMoana/main/config/schedule.wpi
+sudo cp config/schedule.wpi /home/pi/wittypi/schedule.wpi
+
 echo "KaniMoana scripts updated successfully."
